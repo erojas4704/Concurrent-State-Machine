ef74dc3d0399d7c803d9d85f14229378d1b8390e
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2023-01-17 17:09:53 -0500

    Expunged unused code. Updated package.json

--- Assets/Scripts/Actor/Playground/Player.cs
+++ Assets/Scripts/Actor/Playground/Player.cs
@@ -7,33 +7,13 @@ namespace playground
     public class Player : MonoBehaviour
     {
         private Actor actor;
+
         private void Start()
         {
-            actor = GetComponent<Actor>();            
+            actor = GetComponent<Actor>();
             actor.EnterState<Airborne>();
             actor.EnterState<MeleeArmed>();
 
         }
-
-        /*
-        public void OnTriggerEnter(Collider other)
-        {
-            if (other.GetComponent<Ladder>() != null)
-            {
-                //TODO, forward triggers to states and let them handle them by name
-                //Try to reduce as much logic here as possible
-                actor.PropagateAction(new Action("Ladder", other.GetComponent<Ladder>()), false);
-            }
-        }
-
-        public void OnTriggerExit(Collider other)
-        {
-            if (other.GetComponent<Ladder>() != null)
-            {
-                actor.PropagateAction(new Action("Ladder", other.GetComponent<Ladder>(), Action.ActionPhase.Released),
-                    false);
-            }
-        }
-        */
     }
 }
