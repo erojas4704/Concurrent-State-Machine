22174cac17eb150c0b42dd618c6bd22901b4e49b
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2023-07-11 16:43:26 -0400

    Editor now properly renders inherited stats. Inherited stats should now properly reset. We'll see.
    Published artifact 0.5.2

--- Assets/Editor/CSM/StatsEditor.cs
+++ Assets/Editor/CSM/StatsEditor.cs
@@ -1,5 +1,6 @@
 using CSM;
 using System;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
 
@@ -16,10 +17,9 @@ public class StatsEditor : Editor
     {
         Stats stats = target as Stats;
         Type type = stats!.GetType();
-        FieldInfo[] publicFields =
-            type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic); //BindingFlags.Public |
+        FieldInfo[] privateFields = GetAllFields(type);
 
-        foreach (FieldInfo field in publicFields)
+        foreach (FieldInfo field in privateFields)
         {
             if (field.Name[0] == '_')
             {
@@ -29,8 +29,7 @@ public class StatsEditor : Editor
                 if (statObject != null && statObject.GetType().IsGenericType &&
                     statObject.GetType().GetGenericTypeDefinition() == typeof(Stats.Stat<>))
                 {
-                    FieldInfo originalValueField =
-                        type.GetField(statName, BindingFlags.Instance | BindingFlags.NonPublic);
+                    FieldInfo originalValueField = GetFieldIncludingBaseTypes(type, statName);
                     MethodInfo getValueMethod = statObject.GetType().GetMethod("GetValue");
 
                     object originalValue = originalValueField!.GetValue(stats);
@@ -41,6 +40,32 @@ public class StatsEditor : Editor
             }
             //EditorGUILayout.Space();
         }
+    }
+    
+    private static FieldInfo[] GetAllFields(Type type)
+    {
+        if (type == null)
+            return new FieldInfo[0];
+
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly;
+        return type.GetFields(flags)
+            .Concat(GetAllFields(type.BaseType))
+            .ToArray();
+    }
+    
+    private static FieldInfo GetFieldIncludingBaseTypes(Type type, string name)
+    {
+        FieldInfo field = null;
+        while (type != null)
+        {
+            field = type.GetField(name, BindingFlags.Instance | BindingFlags.NonPublic);
+            if (field != null)
+                break;
+
+            type = type.BaseType;
+        }
+
+        return field;
     }
 
     private string ExtractStatName(string fieldName)
