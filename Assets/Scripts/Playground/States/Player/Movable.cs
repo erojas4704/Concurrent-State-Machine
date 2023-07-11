070041036f7483d79f4f018d0e9ed1f221b57002
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2023-07-11 14:38:44 -0400

    CSMP Changes.

--- Assets/Scripts/Playground/States/Player/Movable.cs
+++ Assets/Scripts/Playground/States/Player/Movable.cs
@@ -11,6 +11,7 @@ namespace Playground.States.Player
         private PlayerActor player;
         protected CharacterController controller;
         private Vector3 vel;
+        private const float DIRECTION_CHANGE_THRESHOLD = 0.99f;
 
         public override void Init(Message initiator)
         {
@@ -21,31 +22,44 @@ namespace Playground.States.Player
         public override void Update()
         {
             //TODO demo: We shouldn't use the axis here.
-            
-             Vector3 targetVelocity = new()
+
+            Vector3 targetVelocity = new()
             {
                 x = player.axis.x * stats.Speed,
                 z = player.axis.y * stats.Speed
             };
-            
-             //Target Velocity will be 5 if we are airborne from a Sprint. Our base velocity will be 8.
-             //in this case we should ignore target velocity and use drag.
+
+
+            //Target Velocity will be 5 if we are airborne from a Sprint. Our base velocity will be 8.
+            //in this case we should ignore target velocity and use drag.
 
             //Use a flat version of our movement vector so the Y axis doesn't factor into the length calculation.
             Vector3 planarVelocity = actor.velocity;
             planarVelocity.y = 0;
 
+            float directionDifferenceFactor = Vector3.Dot(planarVelocity.normalized, targetVelocity.normalized);
+            float accelerationFactor;
+
+            //TODO this implementation isn't great.
             //Figure to whether to use friction or acceleration, based on whichever is greater.
             //If the target velocity is greater than current velocity, we will use acceleration.
-            //Otherwise, we will use friction to decelerate.
-            
+            //Otherwise, we will use friction to decelerate.W
+
             //The issue lies in situations where we need to turn around and our speed is temporarily increased by an action, such as sprinting.
             //The max speed will be set to 5, but our own speed is greater than that. If we want to turn completely around and go 5 in the other
             //direction, it will use friction, because 5 is less than the applied sprinting speed of 8. 
             //We need to incorporate the difference in axis somehow. 
-            float accelerationFactor = targetVelocity.magnitude > planarVelocity.magnitude
-                ? stats.Acceleration
-                : stats.Friction;
+
+            if (directionDifferenceFactor >= DIRECTION_CHANGE_THRESHOLD)
+            {
+                accelerationFactor = targetVelocity.magnitude > planarVelocity.magnitude
+                    ? stats.Acceleration
+                    : stats.Friction;
+            }
+            else
+            {
+                accelerationFactor = stats.Acceleration;
+            }
 
             //Every update, apply the acceleration * time to speed.
             float accelerationThisFrame = accelerationFactor * Time.deltaTime;
@@ -62,6 +76,7 @@ namespace Playground.States.Player
                 player.axis = message.axis;
                 message.processed = true;
             }
+
             return base.Process(message);
         }
 
