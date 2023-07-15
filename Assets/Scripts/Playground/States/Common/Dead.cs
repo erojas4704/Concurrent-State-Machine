59a48ad88940cd7b5455be3ccba63cb6759300f0
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2023-07-15 15:22:06 -0400

    Dead state now respawns player after 5 seconds.

--- Assets/Scripts/Playground/States/Common/Dead.cs
+++ Assets/Scripts/Playground/States/Common/Dead.cs
@@ -8,15 +8,33 @@ namespace Playground.States.Common
     [UsedImplicitly]
     public class Dead : State
     {
+        private Rigidbody rb;
+
         public override void Init(Message initiator)
         {
-            Rigidbody rb = actor.GetComponent<Rigidbody>();
+            rb = actor.GetComponent<Rigidbody>();
             if (rb == null) rb = actor.gameObject.AddComponent<Rigidbody>();
             rb.useGravity = true;
             rb.isKinematic = false;
             rb.constraints = RigidbodyConstraints.None;
             rb.AddForce(actor.transform.forward * 10, ForceMode.Impulse);
             rb.AddForce(actor.transform.up * 10, ForceMode.Impulse);
+        }
+
+        public override void Update()
+        {
+            if (Timer > 5f)
+            {
+                Exit();
+            }
+        }
+
+        public override void End()
+        {
+            Object.Destroy(rb);
+            Transform transform = actor.transform;
+            transform.position = new(6.5f, 2f, -17f);
+            transform.rotation = Quaternion.identity;
         }
     }
 }
